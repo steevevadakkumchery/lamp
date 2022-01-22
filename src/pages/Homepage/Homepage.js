20ee0606e9c17b1e0259f93618e1748542e6662f
Steeve Vadakkumchery <45942491+steevevadakkumchery@users.noreply.github.com>
2022-01-22 15:00:02 +0000

    Story/3 adding data functionality (#3)
    * added server calls for locations
    
    * added extra line before end of file
    
    Co-authored-by: Steeve Vadakkumchery <steeve.vadakkumchery@vodafone.com>

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,23 +1,74 @@
+import { useState, useEffect } from "react";
 import LocationListItem from "../../components/molecules/LocationListItem";
 import WeatherSearchBar from "../../components/molecules/WeatherSearchBar";
+import {
+  getCitiesDataAll,
+  getInitialCitiesDataAll,
+} from "../../api/weatherapi";
 import "./Homepage.css";
 
+const DEFAULT_CITY_LIST = ["london", "paris", "sydney"];
+
 function Homepage() {
+  const [searchValue, setSeachValue] = useState("");
+  const [cities, setCities] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getInitialData();
+    setLoading(true);
+  }, []);
+
+  const getInitialData = async () => {
+    const cityDataRes = await getInitialCitiesDataAll(DEFAULT_CITY_LIST);
+    setCities(cityDataRes);
+    setLoading(false);
+  };
+
+  const getSearchData = async (city) => {
+    setLoading(true);
+    const citiesData = await getCitiesDataAll(city);
+    setCities(citiesData);
+    setLoading(false);
+  };
+
+  const handleSearchTextChange = (e) => {
+    setSeachValue(e.target.value);
+  };
+
+  const handleSearch = () => {
+    if (searchValue) {
+      getSearchData(searchValue);
+    } else {
+      setCities([]);
+    }
+  };
+
   return (
-    <div class="page">
-      <div class="section title">
+    <div className="page">
+      <div className="section title">
         <h1>Weather Finder</h1>
       </div>
 
-      <div class="section input">
-        <WeatherSearchBar />
+      <div className="section input">
+        <WeatherSearchBar
+          value={searchValue}
+          handleChange={handleSearchTextChange}
+          handleSearch={handleSearch}
+        />
       </div>
 
-      <div class="section locations">
-        <h2 class="header">Locations</h2>
-        <ul>
-          <LocationListItem />
-        </ul>
+      <div className="section locations">
+        <h2 className="header">Locations</h2>
+        {loading ? (
+          <h1>Loading . . .</h1>
+        ) : (
+          <ul>
+            {cities.map(({ title, woeid }) => {
+              return <LocationListItem key={woeid} name={title} id={woeid} />;
+            })}
+          </ul>
+        )}
       </div>
     </div>
   );
