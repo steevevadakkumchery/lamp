6581fc53e14d12ad64c7d617277e6f2edf0ec28c
Steeve Vadakkumchery <45942491+steevevadakkumchery@users.noreply.github.com>
2022-01-22 15:56:42 +0000

    integrated the weather list item (#4)
    * integrated the weather list item
    
    * fixed variable inconsistency
    
    Co-authored-by: Steeve Vadakkumchery <steeve.vadakkumchery@vodafone.com>

--- src/components/molecules/LocationListItem/LocationListItem.js
+++ src/components/molecules/LocationListItem/LocationListItem.js
@@ -1,14 +1,32 @@
 import "./LocationListItem.css";
 import WeatherIcon from "../../atoms/WeatherIcon";
-function LocationListItem({ name }) {
+function LocationListItem({
+  title: city,
+  parent,
+  consolidated_weather: weatherData,
+}) {
+  const country = parent.title;
+  const todayWeather = weatherData[0];
+  const {
+    weather_state_abbr: currentWeatherState,
+    the_temp: currentTemperature,
+  } = todayWeather;
+
   return (
     <li>
       <div className="location-item">
         <div>
-          <span>{name}</span>
-          <span>See more</span>
+          <span>
+            {city}, {country}
+          </span>
         </div>
-        <WeatherIcon src="https://via.placeholder.com/50" alt="weather icon" />
+        <div className="temperature-and-icon-container">
+          <span className="temperature">
+            {Math.round(currentTemperature)}
+            <span dangerouslySetInnerHTML={{ __html: "&deg;" }} />
+          </span>
+          <WeatherIcon weatherState={currentWeatherState} />
+        </div>
       </div>
     </li>
   );
